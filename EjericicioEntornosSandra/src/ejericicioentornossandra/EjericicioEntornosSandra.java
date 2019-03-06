47bb7778ecfd1468ef42382049751c6eb635fc5a
sandra <1_web9_29@iesjovellanos.org>
2019-03-06 16:56:53 +0100

    primera version master

---
+++ EjericicioEntornosSandra/src/ejericicioentornossandra/EjericicioEntornosSandra.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejericicioentornossandra;
+
+/**
+ *
+ * @author sandr
+ */
+public class EjericicioEntornosSandra {
+    public void run() {
+        System.out.print("Primera version master");
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new EjericicioEntornosSandra().run();
+        // TODO code application logic here
+    }
+    
+}
